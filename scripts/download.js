f958cb8332d1ec2379fb2970ae21b8306d5c1dc2
pawa <super-pasha@inbox.ru>
2022-06-01 17:08:39 +0300

    NEWS

--- scripts/download.js
+++ scripts/download.js
@@ -3,4 +3,11 @@ var menuSwitch = true;
 function showMenu(){
     menu.classList.toggle("navbar-content-show");
     menu.classList.toggle("navbar-content-hide");
+    if (menu.childNodes[1].style.display == "none")
+    {
+        menu.childNodes[1].style.display = "block"
+    } else
+    {
+        setTimeout(function(){menu.childNodes[1].style.display = "none"}, 500);
+    }
 }
--- scripts/script.js
+++ scripts/script.js
@@ -9,6 +9,12 @@
     var prikol = document.querySelector(".prikol");
     var prikol2 = document.querySelector(".prikol2");
     var prikol3 = document.querySelector(".prikol3");
+    var countdown = 0;
+    var countdownNumberEl = document.getElementById('countdown-number');
+    var circleText = document.getElementById('circleButtonText');
+    var countdownEl = document.getElementById('countdown');
+    var circleEl = document.getElementById('circleButton');
+
     function play() {
         myVideo.play();
         peder2.classList.remove("inline-4");
@@ -16,6 +22,19 @@
         peder.classList.remove("inline-3");
         peder.classList.add("inline-2");
         black.classList.add("blackScreen");
+        countdown = 10;
+        countdownNumberEl.textContent = countdown;
+        countdownEl.classList.remove("countdown");
+        setInterval(function() {
+            countdown -= 1;
+            if (countdown <= 0)
+            {
+                countdownEl.style.display = "none";
+                circleEl.classList.remove("circleButton");
+                circleText.textContent = "X";
+            }
+            countdownNumberEl.textContent = countdown;
+        }, 1000);
         function setOpacity1() {
             ad1.style.opacity = "1";
         }
@@ -38,3 +57,8 @@
         prikol2.style.top = adScale * 100+"%";
         prikol2.style.right = adScale2 * 100+"%";
     }
+
+
+
+
+
